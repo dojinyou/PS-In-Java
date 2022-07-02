b20160c9533680f4e83aceb0fa9295a240548fed
dojinyou <djyou128@gmail.com>
2022-07-02 16:01:38 +0900

    :sparkles: 프로그래머스 백엔드 데브매칭 기출 풀이 (로또)

---
+++ problemsolving/problem-solving/src/main/java/programmers/devmatching/backend21/ProblemLotto.java
@@ -0,0 +1,52 @@
+package programmers.devmatching.backend21;
+
+public class ProblemLotto {
+  // Source : https://programmers.co.kr/learn/courses/30/lessons/77484
+  private static final int MAX_RANK = 6;
+
+  public int[] solution(int[] lottos, int[] winNums) {
+    // 0의 개수 구하기
+    int numOfZero = getCountZeroNumbers(lottos);
+
+    // 최소 매칭 개수 구하기.
+    int numOfMinMatchingNumber = getCountMatchedNumbers(lottos, winNums);
+
+    int expectedMaxRank = getRank(numOfMinMatchingNumber);
+    int expectedMinRank = getRank(numOfMinMatchingNumber + numOfZero);
+
+    return new int[]{ expectedMinRank, expectedMaxRank};
+  }
+
+  private int getRank(int numOfMinMatchingNumber) {
+    if (numOfMinMatchingNumber == 0) {
+      numOfMinMatchingNumber++;
+    }
+    return MAX_RANK - numOfMinMatchingNumber + 1;
+  }
+
+  private int getCountZeroNumbers(int[] lottos) {
+    final int targetNumber = 0;
+    int count = 0;
+    for (int lottoNumber : lottos) {
+      if (lottoNumber == targetNumber) {
+        count++;
+      }
+    }
+    return count;
+  }
+
+  private int getCountMatchedNumbers(int[] lottos, int[] winNumbers) {
+    int matchedNumber = 0;
+
+    for (int lottoNumber: lottos) {
+      for (int winNumber: winNumbers) {
+        if (lottoNumber == winNumber) {
+          matchedNumber++;
+          break;
+        }
+      }
+    }
+
+    return matchedNumber;
+  }
+}
