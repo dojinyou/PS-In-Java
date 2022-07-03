a476e2f5bb7def4335a7b4e9a22c915caf5ce41a
dojinyou <djyou128@gmail.com>
2022-07-03 23:08:32 +0900

    :sparkles: ps 공부

---
+++ problemsolving/problem-solving/src/test/java/programmers/devmatching/backend22/Problem1Test.java
@@ -0,0 +1,12 @@
+package programmers.devmatching.backend22;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Problem1Test {
+
+  @Test
+  void solution() {
+  }
+}
