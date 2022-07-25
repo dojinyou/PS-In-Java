6b59a3abad0c47786b4a9309a9a530edc51f9499
dojinyou <djyou128@gmail.com>
2022-07-26 06:56:18 +0900

    solve: 같은 숫자는 싫어(15분)

---
+++ src/main/java/programmers/programmers/highscorekit/stackqueue/HateSameNumber.java
@@ -0,0 +1,22 @@
+package programmers.programmers.highscorekit.stackqueue;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class HateSameNumber {
+  public int[] solution(int[] numbers) {
+    return removeContinousNumber(numbers);
+  }
+
+  private int[] removeContinousNumber(int[] numbers) {
+    Deque<Integer> deque = new LinkedList<>();
+
+    for (int number: numbers) {
+      if (deque.isEmpty() || deque.peekLast() != number) {
+        deque.add(number);
+      }
+    }
+
+    return deque.stream().mapToInt(Integer::intValue).toArray();
+  }
+}
