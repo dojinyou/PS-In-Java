4d98710e2e99a93686d493ac3a6505a0240fc8c9
dojinyou <djyou128@gmail.com>
2022-08-21 14:30:07 +0900

    solve: 피로도(레벨2, 블로그 참고)

--- src/main/java/programmers/programmers/highscorekit/bruteforce/Fatigue.java
+++ src/main/java/programmers/programmers/highscorekit/bruteforce/Fatigue.java
@@ -2,50 +2,40 @@ package programmers.programmers.highscorekit.bruteforce;
 
 public class Fatigue {
 
-  public static final int MIN_NEED_FATIGUE_IDX = 0;
-  public static final int COST_FATIGUE_IDX = 1;
+  public final int MIN_NEED_FATIGUE_IDX = 0;
+  public final int COST_FATIGUE_IDX = 1;
+  public int[][] dungeons;
+  public boolean[] isVisited;
+
+  public int maxDepth = 0;
 
   public int solution(
       int initFatigue,
       int[][] dungeonInfos
   ) {
-    return getBestRoutine(
-        initFatigue,
-        dungeonInfos,
-        new boolean[dungeonInfos.length],
-        new boolean[dungeonInfos.length],
-        0
-    );
+
+    dungeons = dungeonInfos;
+    isVisited = new boolean[dungeonInfos.length];
+
+    findMaxDepth(initFatigue, 0);
+
+    return maxDepth;
   }
 
-  private int getBestRoutine(
+  private void findMaxDepth(
       int fatigue,
-      int[][] dungeonInfos,
-      boolean[] isVisited,
-      boolean[] isClear,
       int depth
   ) {
-    if (depth == dungeonInfos.length) {
-      return 0;
-    }
 
-    for (int i = 0; i < dungeonInfos.length; i++) {
-      if (!isVisited[i]) {
-        depth++;
+    for (int i = 0; i < dungeons.length; i++) {
+      if (!isVisited[i] && dungeons[i][MIN_NEED_FATIGUE_IDX] <= fatigue) {
         isVisited[i] = true;
-        int result1 = 0;
-        int result2 = getBestRoutine(fatigue, dungeonInfos, isVisited, isClear, depth);
-
-        if (dungeonInfos[i][MIN_NEED_FATIGUE_IDX] <= fatigue) {
-          isClear[i] = true;
-          int nextFatigue = fatigue - dungeonInfos[i][COST_FATIGUE_IDX];
-          result1 = getBestRoutine(nextFatigue, dungeonInfos, isVisited, isClear, depth) + 1;
-        }
-
-        return Math.max(result1, result2);
+        int afterFatigue = fatigue - dungeons[i][COST_FATIGUE_IDX];
+        findMaxDepth(afterFatigue, depth + 1);
+        isVisited[i] = false;
       }
     }
 
-    return 0;
+    maxDepth = Math.max(maxDepth, depth);
   }
 }
