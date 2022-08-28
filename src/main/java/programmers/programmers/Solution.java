be78c70b585342163e16da14481b1d19c54f86ae
dojinyou <djyou128@gmail.com>
2022-08-28 15:16:24 +0900

    solve: 모의테스트 2주차 1차 1번(10분)

--- src/main/java/programmers/programmers/Solution.java
+++ src/main/java/programmers/programmers/Solution.java
@@ -1,11 +1,8 @@
 package programmers.programmers;
 
 class Solution {
-
-  public int solution(
-      int[] ingredient,
-      int results
-  ) {
-    return 1;
+  public int solution(int[] absolutes, boolean[] signs) {
+    int answer = 123456789;
+    return answer;
   }
 }
