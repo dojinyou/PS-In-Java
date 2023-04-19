244f29440825d204677b96b5153dfaf65b85631e
dojinyou <djyou128@gmail.com>
2023-04-19 22:42:18 +0900

    [solve]leetcode - 234. Palindrome Linked List(easy)

--- src/main/java/leetcode/studyplan/level2/day3/RemoveNthNodeFromEndOfList.java
+++ src/main/java/leetcode/studyplan/level2/day3/RemoveNthNodeFromEndOfList.java
@@ -43,14 +43,6 @@ public class RemoveNthNodeFromEndOfList {
         return removeNode;
     }
 
-    private static class ListNode {
-        int val;
-        ListNode next;
-        ListNode() {}
-        ListNode(int val) { this.val = val; }
-        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-    }
-
     public static void main(String[] args) {
         var head = new ListNode(1);
 //        var cur = head;
