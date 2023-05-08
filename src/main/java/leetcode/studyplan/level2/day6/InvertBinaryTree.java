4f39d4c8e9b546e44d6edde186ed6429e8c2bcc4
dojinyou <djyou128@gmail.com>
2023-05-08 12:47:31 +0900

    [solve]leetcode - 226. Invert Binary Tree(easy)

---
+++ src/main/java/leetcode/studyplan/level2/day6/InvertBinaryTree.java
@@ -0,0 +1,19 @@
+package leetcode.studyplan.level2.day6;
+
+import leetcode.studyplan.level2.datastructure.TreeNode;
+
+public class InvertBinaryTree {
+
+	public TreeNode invertTree(TreeNode root) {
+		if (root == null || (root.left == null && root.right == null)) return root;
+
+		var leftNode = root.left;
+		var rightNode = root.right;
+
+		root.right = invertTree(leftNode);
+		root.left = invertTree(rightNode);
+
+		return root;
+	}
+
+}
