e06183fc24381114008d67ae2d68fbdea222b1c1
dojinyou <djyou128@gmail.com>
2023-05-08 13:32:28 +0900

    [solve]leetcode - 110. Balanced Binary Tree(easy)

---
+++ src/main/java/leetcode/studyplan/level2/day6/BalancedBinaryTree.java
@@ -0,0 +1,27 @@
+package leetcode.studyplan.level2.day6;
+
+import leetcode.studyplan.level2.datastructure.TreeNode;
+
+public class BalancedBinaryTree {
+
+	public boolean isBalanced(TreeNode root) {
+		if (root == null || (root.right == null && root.left == null)) return true;
+		if (!isBalanced(root.left) || !isBalanced(root.right)) return false;
+
+		var leftHeight = getHeight(root.left);
+		var rightHeight = getHeight(root.right);
+
+		return Math.abs(leftHeight - rightHeight) < 2;
+	}
+
+	private int getHeight(TreeNode root) {
+		if (root == null) return 0;
+		if (root.left == null && root.right == null) return 1;
+
+		var leftHeight = getHeight(root.left);
+		var rightHeight = getHeight(root.right);
+
+		return Math.max(leftHeight, rightHeight) + 1;
+	}
+
+}
